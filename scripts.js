3c2176cd3e0a4005a33eea24c1d17fbc0f7f2691
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-02-08 14:47:37 -0600

    added random color dblclick to h1-h6.

--- scripts.js
+++ scripts.js
@@ -43,7 +43,40 @@ h6.className = "h6";
 div.appendChild(h6);
 document.body.appendChild(div);
 
+let colors = [
+    'red', 
+    'green',
+    'blue', 
+    'cyan', 
+    'magenta', 
+    'yellow', 
+    'black',
+    'purple'
+];
 
+h1.addEventListener("dblclick", function () {
+    h1.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+h2.addEventListener("dblclick", function () {
+    h2.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+h3.addEventListener("dblclick", function () {
+    h3.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+h4.addEventListener("dblclick", function () {
+    h4.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+h5.addEventListener("dblclick", function () {
+    h5.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+h6.addEventListener("dblclick", function () {
+    h6.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
 
 });
 
